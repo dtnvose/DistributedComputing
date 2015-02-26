7321bc020345e04e4d31915618b5bb17f2ec8a81
Dustin Vose <dtnvose@gmail.com>
2015-02-25 22:16:36 -0500

    Server and SThread can retrieve any file type now

--- src/SThread.java
+++ src/SThread.java
@@ -1,13 +1,15 @@
 import java.io.*;
 import java.net.*;
+import java.util.Arrays;
 import java.lang.Exception;
+
 
 	
 public class SThread extends Thread 
 {
 	private Object [][] RTable; // routing table
-	private PrintWriter out, outTo; // writers (for writing back to the machine and to destination)
-   private BufferedReader in; // reader (for reading from the machine connected to)
+	private DataOutputStream out, outTo; // writers (for writing back to the machine and to destination)
+   private DataInputStream in; // reader (for reading from the machine connected to)
 	private String inputLine, outputLine, destination, addr; // communication strings
 	private Socket outSocket; // socket for communicating with a destination
 	private int ind; // indext in the routing table
@@ -15,8 +17,8 @@ public class SThread extends Thread
 	// Constructor
 	SThread(Object [][] Table, Socket toClient, int index) throws IOException
 	{
-			out = new PrintWriter(toClient.getOutputStream(), true);
-			in = new BufferedReader(new InputStreamReader(toClient.getInputStream()));
+			out = new DataOutputStream(toClient.getOutputStream());
+			in = new DataInputStream(toClient.getInputStream());
 			RTable = Table;
 			addr = toClient.getInetAddress().getHostAddress();
 			RTable[index][0] = addr; // IP addresses 
@@ -30,9 +32,10 @@ public class SThread extends Thread
 		try
 		{
 		// Initial sends/receives
-		destination = in.readLine(); // initial read (the destination for writing)
+		destination = in.readUTF(); // initial read (the destination for writing)
 		System.out.println("Forwarding to " + destination);
-		out.println("Connected to the router."); // confirmation of connection
+		out.writeUTF("Connected to the router.");
+		// confirmation of connection
 		
 		// waits 10 seconds to let the routing table fill with all machines' information
 		try{
@@ -48,20 +51,19 @@ public class SThread extends Thread
 					if (destination.equals((String) RTable[i][0])){
 						outSocket = (Socket) RTable[i][1]; // gets the socket for communication from the table
 						System.out.println("Found destination: " + destination);
-						outTo = new PrintWriter(outSocket.getOutputStream(), true); // assigns a writer
+						outTo = new DataOutputStream(outSocket.getOutputStream()); // assigns a writer
 				}}
 		
-		// Communication loop	
-		while ((inputLine = in.readLine()) != null) {
-            System.out.println("Client/Server said: " + inputLine);
-            if (inputLine.equals("Bye.")) // exit statement
-					break;
-            outputLine = inputLine; // passes the input from the machine to the output string for the destination
-				
-				if ( outSocket != null){				
-				outTo.println(outputLine); // writes to the destination
-				}			
-       }// end while		 
+		// Communication loop
+		byte[] data = new byte[1000000];
+		int count = -1;
+		while ((count = in.read(data)) != -1 && count != 3) {
+				if (outSocket != null){
+					System.out.println(count);
+					outTo.write(data,0,count);
+				}
+       }// end while
+		System.out.println("Client/Server Done");
 		 }// end try
 			catch (IOException e) {
                System.err.println("Could not listen to socket.");
